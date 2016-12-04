27e8c604c09576cbb7e71b7fb6121f6ae0a835c8
Bryan Jones <thebeard@me.com>
2016-12-03 23:53:00 -0800

    Simple Error reporting for registration

--- server.py
+++ server.py
@@ -14,25 +14,29 @@ def index():
 @app.route('/validate', methods=['POST'])
 def validate():
 
+    validation = 'Success'
+
     if len(request.form['email'])<1:
         flash("Email cannot be blank!")
-        print "email blank"
+        validation = 'False'
+
     elif not EMAIL_REGEX.match(request.form['email']):
         flash('Invalid Email Address!')
-        print "invalid email"
-
+        validation = 'False'
     if len(request.form['first_name'])<1 or not ALPHA_REGEX.match(request.form['first_name']):
             flash('Invalid First Name!')
-            print "first name bad"
+            validation = 'False'
     if len(request.form['last_name'])<1 or not ALPHA_REGEX.match(request.form['last_name']):
             flash('Invalid Last Name!')
-            print "last name bad"
+            validation = 'False'
     if len(request.form['password'])<=8:
         flash('Password must be longer than 8 characters!')
-        print 'less than 8'
+        validation = 'False'
     if request.form['password']!=request.form['conf_password']:
         flash('Password does not match!')
-        print 'invalid password'
+        validation = 'False'
+    if validation == 'Success':
+        flash ('Thanks for submitting your information')
 
     return redirect('/')
 
